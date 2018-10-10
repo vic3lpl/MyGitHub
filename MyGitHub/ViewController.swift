6379ab0d5fe5b8ceb6373070b11a4019a646e5c4
Lim Ai Zhi <aizhi.lim@sicmsb.com>
2018-10-10 10:11:00 +0800

    infiniti github test

--- MyGitHub/ViewController.swift
+++ MyGitHub/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("My infiniti gihub test");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
