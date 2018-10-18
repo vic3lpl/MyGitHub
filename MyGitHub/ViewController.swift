75dd3c4d4d0db2910c503b4eb68d55a8a6fe0b2d
Lim Ai Zhi <aizhi.lim@sicmsb.com>
2018-10-18 15:58:36 +0800

    another merge

--- MyGitHub/ViewController.swift
+++ MyGitHub/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var i:Int = 0;
         print("My infiniti gihub test");
 
 
@@ -20,6 +21,11 @@ class ViewController: UIViewController {
         print("success merge from dev to master dev");
 
         print("try again ui conflict");
+        
+        if i == 0 {
+            i = 1;
+            print("i am dev lai");
+        }
 
 
         // Do any additional setup after loading the view, typically from a nib.
